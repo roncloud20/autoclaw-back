480ffe86849bc405d671375a6c099b228c16297e
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2026-03-30 11:00:40 +0100

    Redirection to frontend

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('https://autoclaw-front.vercel.app');
 });
 
 Route::get('/home', function () {
